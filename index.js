30b0aec62251fb16b5651d458b3ac8c43b07d1dd
Yevhen <yevhen.prybytok@binary-studio.com>
2022-02-08 10:51:52 +0200

    Project init

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from 'src/app';
-import {name as appName} from 'src/app-info.json';
+import { AppRegistry } from 'react-native'
+import App from 'src/app'
+import { name as appName } from 'src/app-info.json'
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => App)
