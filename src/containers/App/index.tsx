4412042b36b010ff1c6d3e26af13163655521a8c
R-Mielamud <2m.roman2@gmail.com>
2022-02-08 15:57:14 +0200

    Added redux provider to App container

--- src/containers/App/index.tsx
+++ src/containers/App/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { SafeAreaView, Text } from 'react-native'
+import { Provider as ReduxProvider } from 'react-redux'
+import store from 'src/redux/store'
 
 import styles from 'src/containers/App/styles'
 
 const App = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.greetings}>Hello There!</Text>
+      <ReduxProvider store={store}>
+        <Text style={styles.greetings}>Hello There!</Text>
+      </ReduxProvider>
     </SafeAreaView>
   )
 }
